0cb76651dded88ef9419ad1f9479fda327bc2491
gpy1234abcd <56966692+gpy1234abcd@users.noreply.github.com>
2025-10-24 16:44:25 +0800

    Update download_dataset.py

--- download_dataset.py
+++ download_dataset.py
@@ -1,9 +1,9 @@
 import os, gdown, zipfile
-
+#https://drive.google.com/file/d/1mq3T-wiTsSU_8ucqK-2IIoJhJNKLGvlX/view?usp=drive_link
 FILE_ID = "你的FILE_ID"  # 
 
 print("下载中...")
-gdown.download(f"https://drive.google.com/uc?id={FILE_ID}", 
+gdown.download(f"https://drive.google.com/uc?id={d/1mq3T-wiTsSU_8ucqK-2IIoJhJNKLGvlX/view?usp=drive_link}", 
                "KITTI——gong.zip", quiet=False)
 
 print("解压中...")
