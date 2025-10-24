e1c8e749f9fd75a0c8af36a3ff3bc836ce777c51
gpy1234abcd <56966692+gpy1234abcd@users.noreply.github.com>
2025-10-24 16:49:35 +0800

    Update download_dataset.py

--- download_dataset.py
+++ download_dataset.py
@@ -1,9 +1,9 @@
 import os, gdown, zipfile
 #https://drive.google.com/file/d/1mq3T-wiTsSU_8ucqK-2IIoJhJNKLGvlX/view?usp=drive_link
-FILE_ID = "你的FILE_ID"  # 
+FILE_ID = "1mq3T-wiTsSU_8ucqK-2IIoJhJNKLGvlX"
 
 print("下载中...")
-gdown.download(f"https://drive.google.com/uc?id={d/1mq3T-wiTsSU_8ucqK-2IIoJhJNKLGvlX/view?usp=drive_link}", 
+gdown.download(f"https://drive.google.com/uc?id={FILE_ID}", 
                "KITTI——gong.zip", quiet=False)
 
 print("解压中...")
@@ -11,4 +11,4 @@ with zipfile.ZipFile("KITTI——gong.zip", 'r') as z:
     z.extractall(".")
 os.remove("KITTI——gong.zip")
 
-# print("✓ 完成！数据集在 ./VOCdevkitq/VOC2007/")
+print("✓ 完成！数据集已解压到当前目录")
